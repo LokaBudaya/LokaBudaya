d9b5af14b69783d2fa11beb922e80fb5cd53694b
Adika Nugraha <ibanana@192.168.1.21>
2025-05-08 18:08:47 +0700

    scroll profile

--- app/src/main/java/com/dev/lokabudaya/pages/ProfilePage.kt
+++ app/src/main/java/com/dev/lokabudaya/pages/ProfilePage.kt
@@ -11,8 +11,10 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.*
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
@@ -32,6 +34,7 @@ fun ProfilePage() {
             .fillMaxSize()
             .background(Color(0xFFF8F8F8))
             .padding(horizontal = 16.dp)
+            .verticalScroll(rememberScrollState())
     ) {
         Spacer(modifier = Modifier.height(32.dp))
         Row(
