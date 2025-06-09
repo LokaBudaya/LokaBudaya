44404b94950b88d62df317a930b6d9c47a71df2f
adika <adikanugraha2425@student.uns.ac.id>
2025-06-09 10:46:22 +0700

    a

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,6 +56,10 @@ dependencies {
     implementation("com.google.android.libraries.places:places:3.3.0")
     implementation("com.google.android.gms:play-services-location:21.0.1")
     implementation("com.google.android.gms:play-services-maps:18.2.0")
+    implementation("com.midtrans:uikit:2.0.0")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.9.3")
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
